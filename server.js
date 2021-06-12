75b11e14e886bcd0926a4c34eb4238990bf9a622
Ethan McDowell <ethan.r.mcdowell@gmail.com>
2021-06-12 12:34:03 -0400

    Still working on heroku deploy issues

--- server.js
+++ server.js
@@ -17,9 +17,9 @@ mongoose.connect(process.env.MONGODB_URI, {
 const path = require('path');
 const publicPath = path.join(__dirname, '/client/public');
 app.use(express.static(publicPath));
-app.get('*', (req, res) => {
-  res.sendFile(path.join(publicPath, 'index.html'));
-});
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(publicPath, 'index.html'));
+// });
 
 // if (process.env.NODE_ENV === 'production') {
 //   app.use(express.static('client/build'));
